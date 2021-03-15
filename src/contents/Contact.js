b44d9f97106a1f89948ab076936398fae6c14797
KnutHendrikS <70657338+KnutHendrikS@users.noreply.github.com>
2021-03-15 09:45:19 +0200

    small changes

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -1,15 +1,14 @@
 import './Contact.css';
 
 const Contact = () => {
-    return ( 
+    return (
         <div className="contact">
-            <h2  className="title">Contact Me</h2>
-            <br/>
-            <h3  className="secondtext">Email : Hendrik.Schnur16@gmail.com</h3>
-            <h3  className="secondtext">Phone : +358 505818966</h3>
-            <h3  className="secondtext">Location : Finland, Turku</h3>
+            <h2 className="title">Contact Me</h2>
+            <br />
+            <h3 className="secondtext">Email : hendrik.schnur16@gmail.com</h3>
+            <h3 className="secondtext">Phone : +358 505818966</h3>
         </div>
-     );
+    );
 }
- 
+
 export default Contact;
--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -1,35 +1,36 @@
 import './Skills.css';
 
 const Skills = () => {
-    return ( 
-        <div className="skills">
-            <h1>Skills</h1>
-            <div className="skills_content">
-              <div className="col">
-                <h3>Programming / other skills</h3>
-                <ul>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>Javascript</li>
-                  <li>React</li>
-                  <li>Java</li>
-                  <li>Unity/C#</li>
-                  <li>Blender 3D</li>
-                  <li>Photoshop</li>
-                </ul>
-              </div>
-              <div className="col">
-                <h3>Language skills</h3>
-                <ul>
-                  <li>English - Excellent</li>
-                  <li>Finnish - Native tongue</li>
-                  <li>German - Good</li>
-                  <li>Swedish - Fair</li>
-                </ul>
-              </div>
-            </div>
+  return (
+    <div className="skills">
+      <h1>Skills</h1>
+      <div className="skills_content">
+        <div className="col">
+          <h3>Programming / other skills</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>React</li>
+            <li>Java</li>
+            <li>Python</li>
+            <li>Unity/C#</li>
+            <li>Blender 3D</li>
+            <li>Photoshop</li>
+          </ul>
         </div>
-     );
+        <div className="col">
+          <h3>Language skills</h3>
+          <ul>
+            <li>English - Excellent</li>
+            <li>Finnish - Native tongue</li>
+            <li>German - Good</li>
+            <li>Swedish - Fair</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
- 
+
 export default Skills;
